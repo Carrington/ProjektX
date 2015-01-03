3a5df13dfe57c1644d397fa2cf7c8f270d4b8cd3
unknown <daingrah@DAINGRAH-WS01.cisco.com>
2015-01-02 22:09:16 -0500

    UPDATE: Finished early plotManager requirements

--- engine/managers/plotManager.js
+++ engine/managers/plotManager.js
@@ -1,34 +1,58 @@
 //jshint moz:true
-var Bacon = require('./node_modules/Bacon').Bacon;
+var Bacon = require('./node_modules/Bacon').Bacon,
+    commMgr = require('./engine/managers/communicationManager').communicationManager;
 
 let module;
 
-module.exports = {
-  plotManager: (function () {
-    let mgr;
-    
-    /**
-     * Plot Manager examines the merged stream and
-     * directs delegate properties to adjust 
-     * themselves accordingly.  
-     **/
-    mgr = {
-      busStream: new Bacon.Bus(),
-      plugStream: function ( stream ) {
+/**
+ * Plot Manager examines the bus stream and
+ * directs delegate properties to adjust 
+ * themselves accordingly.  
+ **/
+
+module.exports = function plotManager(options) {
+  let busStream = new Bacon.Bus(),
+      plugStream = function ( stream ) {
         if (stream instanceof Bacon.EventStream) {
-          this.busStream.plug(stream);
+          busStream.plug(stream);
         }
       },
-      processEvents: function() { 
-        //figure out how to delegate stuff
-      },
-      destroy: function() {
-        this.busStream.end();
-      }
-    };
-    
-    mgr.busStream.onValue(mgr.processEvents);    
-    return mgr;
+      manageEvent = function ( event ) {
+       let contactMap = new commMgr.ContactMap(event),
+           impacts = contactMap.impacts(),
+           alignReaction = function ( reaction ) {
+            let index;
 
-  })() 
+            if (typeof options.postReactionHooks !== 'undefined') {
+              for (index = 0; index < options.postReactionHooks.length; ++index) {
+                //pre- and post- reaction hooks must return a valid event object,
+                //which is pushed to the combined bus stream.
+                busStream.push(options.postReactionHooks[index]( reaction ));
+              }
+            }
+
+           },
+           destroy = (options.destroy !== 'undefined') ? destroy : options.destroy,
+           plugStream = (options.plugStream !== 'undefined') ? plugStream : options.plugStream,
+           contact,i,index;
+
+       for (i = 0; i < impacts.length; ++i) {
+         
+         contact = impacts[i];
+         if (typeof options.preReactionHooks !== 'undefined') {
+           for (index = 0; index < options.preReactionHooks.length; ++index) {
+             //pre- and post- reaction hooks must return a valid event object,
+             //which is pushed to the combined bus stream.
+             busStream.push(options.preReactionHooks[index]( contact, event ));
+           }
+         }
+         contact.enqueueReaction( event, alignReaction );
+
+       }
+       
+      },
+      destroy = function () {
+        busStream.end();      
+      };
+      return manageEvent;
 };
